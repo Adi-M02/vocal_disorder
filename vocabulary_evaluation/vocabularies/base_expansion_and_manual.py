7d4283cf86393fc719e5a186ac13207127ed4a7a
Adi-M02 <91706393+Adi-M02@users.noreply.github.com>
2025-04-11 04:37:09 -0500

    pipeline to evaluate vocab against ground truths in place

---
+++ vocabulary_evaluation/vocabularies/base_expansion_and_manual.py
@@ -0,0 +1,70 @@
+rcpd_terms = {
+    "Symptom Experience & Severity": [
+        'Burp_stomach_issues', 'anxiety_induced', 'burp_anxiety_issues', 'burps_stay', 'burbs', 'burp_anxiety_issues',
+        'tummy', 'vomit_burps', 'Throwing_up', 'vomiting_work', 'Gurgles', 'Shortness_of_breath', 'vomiting_hold',
+        'vomiting_bad', 'Gurgling', 'Pressure', 'Air_vomiting', 'burps_vomit', 'Vomit', 'get_air_vomiting', 'Hiccups',
+        'Chest_pain', 'vomiting_sound', 'burps_symptoms', 'cramps_gas', 'Vomit_air', 'air_vomiting_throat', 'Croaking',
+        'Gag', 'burps_stomach', 'Croaks', 'Retching', 'Throat_tightness', 'vomiting_pressure', 'hiccups_stomach',
+        'Throat_gurgles', 'air_vomiting_sound', 'Difficulty_breathing', 'Bloating', 'Regurgitation', 'air_vomiting',
+        'dry_heaving', 'vomiting', 'no_burp', 'gagging', 'involuntary_dry_heaving', 'typical_anxiety_symptom',
+        'anaesthesia', 'slow_swallow', 'retching', 'nauseous', 'bubble_feeling', 'excess_wind', 'flatulence',
+        'uncomfortable', 'nauseating', 'vomitting', 'nausea_in_throat', 'expanding_bubble_feeling', 'nauseous',
+        'tummy_aches', 'stomach_nausea', 'zero_appetite', 'dry_heave-y', 'uncontrollable_dry_heaving',
+        'involuntary_gagging', 'globus_sensation', 'globus', 'gagging_involuntarily', 'excessive_bloating',
+        'choking_sensation', 'choking', 'gaggy', 'difficulty_breathing', 'bloating', 'distension', 'full_of_air',
+        'constipation', 'inability_to_burp', 'can’t_burp', 'gas_trapped', 'gas_trapped_pain', 'liquid_vomiting',
+        'early_satiety', 'Abdominal_pain', 'motility_issues', 'unable_to_burp', 'constipation', 'slow_swallowing',
+        'acid_reflux', 'hurts'
+    ],
+
+    "Botox Treatment Experiences & Procedures": [
+        'Throat_tightness', 'sore_throat_etc', 'sore_throat', 'botox_drink', 'botox_swallowing', 'burp_mini',
+        'mini_burp', 'procedure', 'procedure_always', 'procedure_happy', 'in-office_procedure', 'procedure_thing',
+        'procedure_hope', 'procedure_something', 'procedure_talking', 'botox', 'procedure_sore', 'procedure_waited',
+        'procedure_worried', 'procedure_doctor', 'nervous_procedure', 'Injections', 'procedure_often', 'procedure_get',
+        'procedure_trying', 'Anesthesia', 'procedure_first', 'procedure_give', 'procedure_said', 'surgery', 'Units',
+        'procedure_us', 'procedure_help', 'bubble_in_throat', 'relieving_bloating', 'relieving', 'post-botox_burpers',
+        'botox_procedure', 'general_anaesthetic', 'insurance', 'insurance_provider', 'anaesthesia', 'groggy',
+        'surgery_diaries', 'liquid_ibuprofen', 'anti_nausea_medication', 'post_anaesthetic_nausea', 'ear_pain',
+        'wet_burps', 'post-botox', 'throat_tightness', 'Ondansetron', 'Zofran', 'microburps', 'pain', 'inflammation',
+        'uncontrollable_belching', 'lump', 'hurts', 'hyper - salivation', 'hypersalvation', 'micro_burp', 'microburped'
+    ],
+
+    "Self-Treatment & Behavioral Interventions": [
+        'Patting', 'back_work_procedure', 'shaker_exercise', 'traditional_chinese_medicine', 'herbal_treatment',
+        'acupuncture', 'tcm_herbs', 'wedge_pillow', 'fizzy_water', 'carbonated_water', 'head_turn', 'craning_neck_forward',
+        'turning_heading', 'craning_neck', 'positioning', 'contort', 'speech_therapy', 'larynx_lowering_exercise',
+        'carbonated_water', 'CBT', 'EMDR', 'Chamomile', 'Tea', 'Exercises', 'Gas-X', 'Famotidine', 'Fizzy_drinks',
+        'Omeprazole', 'Neck_turning', 'Self-taught', 'Self-curing', 'Shaker', 'Pelvic_tilt', 'Mounjaro', 'antacids',
+        'kiss_the_ceiling', 'Self-cured', 'Rapid_Drink_Challenge', 'cricoid_massage'
+    ],
+
+    "Psychological Factors & Mental Health": [
+        'Sad', 'depressed', 'anxiety_induced', 'isolation', 'isolated', 'anxiety_issue', 'Social-anxiety',
+        'burp_anxiety_issues', 'Anxiety', 'anxiety_cause', 'social_anxiety_also', 'fear_vomiting'
+    ],
+
+    "Alternative Surgical Treatments/Diagnostics": [
+        'Stoma', 'muscle_operation', 'upper_esophageal_sphincter', 'barium_swallow_study', 'excessive_air_swallowing',
+        'Biopsies', 'colonoscopy', 'Upper_GI_endoscope', 'nasal_gastric', 'nasal_gastric_tube', 'Xrays_of_colon',
+        'pH_impedance', 'pH‐impedance_monitoring', 'esophageal_sphincters', 'acid_reflux', 'speech_therapy', 'EMDR',
+        'omeprazole', 'Misdiagnosis', 'Esophageal_dilation', 'ENT', 'Gastroenterologist', 'Laryngologist',
+        'Otolaryngologist', 'PCP', 'Specialist', 'Insurance', 'Endoscopy', 'Nasoendoscopy', 'Swallow_tests',
+        'esophageal_examination', 'HRM', 'Manometry', 'Fluoroscopy', 'Imaging', 'Barium swallow', 'FEES_test', 'FEES'
+    ],
+
+    "Comorbidities": [
+        'Small_Intestine_Bacterial_Overgroeth', 'Small_Intestine_Bacterial_Overgrowth', 'SIBO', 'hiatal_hernia',
+        'aerophagia_on_swallowing', 'Sigmoid_colon', 'nasal_gastric', 'nasal_gastric_tube', 'Xrays_of_colon',
+        'gastroesophageal_gas_reflux', 'supragastric_belching', 'ehlers_danlos_syndrome', 'Abdomino_phrenic_dyssynergia',
+        'APD', 'gi_disorder', 'irritable_bowel_syndrome', 'ibs'
+    ],
+
+    "Improvement indicators (success, relief, reduced symptoms)": [
+        'Cures', 'cure', 'life_changing', 'cured', 'still_burping', 'able_to_burp', 'still_burping', 'burp_frequently'
+    ],
+
+    "Deterioration indicators (worsening symptoms)": [
+        'Hurts', 'pain', 'inflammation', 'bloating', 'ineffective'
+    ]
+}
