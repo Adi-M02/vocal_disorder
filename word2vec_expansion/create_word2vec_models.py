c024cbae31c95394bda89fc052bc3ed4ce68a546
Adi-M02 <91706393+Adi-M02@users.noreply.github.com>
2025-05-25 17:02:54 -0500

    setup to create cbow and skipgram models

--- word2vec_expansion/create_word2vec_models.py
+++ word2vec_expansion/create_word2vec_models.py
@@ -1,5 +1,17 @@
+import logging
+from gensim.models import Word2Vec
 import sys
 
 sys.path.append('../vocal_disorder')
-import query_mongo as query
+from query_mongo import return_documents
+from tokenizer import clean_and_tokenize
 
+
+docs = return_documents("reddit", "noburp_all", ["noburp"])
+print(f"Number of documents: {len(docs)}")
+cleaned_docs = []
+for doc in docs:
+    # Tokenize and clean the text
+    tokens = clean_and_tokenize(doc["noburp"])
+    cleaned_docs.append(tokens)
+print(f"Number of documents: {len(cleaned_docs)}")
