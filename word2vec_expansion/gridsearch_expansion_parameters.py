f19ca45f4f397d19e6fcb3733a0852cddc26ffb4
Mukundan <amukundan@r-lnx217.cs.uiowa.edu>
2025-06-12 10:43:16 -0500

    more efficiently running gridsearch over expansion parameters

--- word2vec_expansion/gridsearch_expansion_parameters.py
+++ word2vec_expansion/gridsearch_expansion_parameters.py
@@ -3,16 +3,28 @@ import itertools
 
 # fixed arguments
 TERMS       = "rcpd_terms_6_5.json"
-MODEL_DIR   = "word2vec_expansion/word2vec_06_06_20_49"
+MODEL_DIR   = "word2vec_expansion/word2vec_06_10_20_41"
 MANUAL_DIR  = "vocabulary_evaluation/updated_manual_terms_6_3"
+METRICS_PATH = f"{MODEL_DIR}/metrics.json"
 SPELLCHECK  = True  # set False to remove --spellcheck
 
 # grid ranges
-TOPK_VALUES  = list(range(25, 80, 5))      # 25,30,...,75
+TOPK_VALUES  = list(range(50, 100, 5))
 FREQ_VALUES  = [i/100 for i in range(15, 0, -1)]  # 0.15,0.14,...,0.01
 
 SCRIPT_PATH = "word2vec_expansion/word2vec_expansion_lemmatized.py"
-
+cmd = [
+        "python", SCRIPT_PATH,
+        "--terms", TERMS,
+        "--model_dir", MODEL_DIR,
+        "--topk", str(45),
+        "--freq_threshold", str(0.01),
+        "--manual_dir", MANUAL_DIR,
+        "--spellcheck", 
+        "--metrics_output", METRICS_PATH
+        
+    ]
+subprocess.run(cmd, check=True)
 for topk, freq in itertools.product(TOPK_VALUES, FREQ_VALUES):
     cmd = [
         "python", SCRIPT_PATH,
@@ -22,6 +34,7 @@ for topk, freq in itertools.product(TOPK_VALUES, FREQ_VALUES):
         "--freq_threshold", str(freq),
         "--manual_dir", MANUAL_DIR,
         "--spellcheck", 
+        "--metrics_output", METRICS_PATH
         
     ]
 
