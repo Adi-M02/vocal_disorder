08cf4f9e6b3f11d8d899cd149dd381265ea037e8
Adi Mukundan <adithyamukundan@gmail.com>
2025-08-13 16:45:03 -0500

    incorporating stoplisting into text processing pipeline

--- utils/text_pipeline.py
+++ utils/text_pipeline.py
@@ -3,7 +3,7 @@ sys.path.append('../vocal_disorder')
 from tokenizer import clean_and_tokenize
 from utils.load_lemmatizer import load_lookup
 from spellchecker_folder.spellchecker import spellcheck_token_list
-
+from utils.stopwords import STOPWORDS
 
 def process_text(text, lookup_path='testing/combined_lemmas.json'):
     "process a dcoument. tokenize -> lemmatize -> retokenize -> spellcheck -> relemmatize -> retokenize"""
@@ -15,6 +15,7 @@ def process_text(text, lookup_path='testing/combined_lemmas.json'):
     toks = clean_and_tokenize(text)
     toks = spellcheck_token_list(toks)
     toks = [lookup_map.get(tok, tok) for tok in toks]
+    toks = [tok for tok in toks if tok not in STOPWORDS]
     text = ' '.join(toks)
     toks = clean_and_tokenize(text)
     return toks
